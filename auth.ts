8a871ab778a2bc75a0049af3e0e081df8a05d6c0
Marl Denzell J. Silva <silvamarldenzell@gmail.com>
2026-05-04 14:52:32 +0800

    feat(auth): add Google OAuth provider to authentication

--- auth.ts
+++ auth.ts
@@ -2,10 +2,11 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub],
+  providers: [GitHub, Google],
   pages: {
     signIn: "/login",
   },
