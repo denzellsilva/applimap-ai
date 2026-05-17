7c90d48377fdd937ca22e4f28001841d72eec74b
Marl Denzell J. Silva <silvamarldenzell@gmail.com>
2026-05-17 18:47:27 +0800

    feat(layout): refactor Layout component structure and change header semantics

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,24 +2,20 @@ import { quantico } from "@/ui/fonts";
 import { LogOut } from "lucide-react";
 import { signOut } from "@/auth";
 
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <header className="ite flex items-center justify-between border-b border-gray-200 p-2 px-4">
-        <h1 className={`${quantico.className} text-3xl`}>
+      <header className="flex items-center justify-between border-b border-gray-200 p-2 px-4">
+        <div id="logo" className={`${quantico.className} text-3xl`}>
           <span className="font-bold text-green-800">Appli</span>Map
-        </h1>
+        </div>
         <form
           action={async () => {
             "use server";
             await signOut();
           }}
         >
-          <button>
+          <button type="submit" aria-label="Sign out" title="Sign out">
             <LogOut />
           </button>
         </form>
